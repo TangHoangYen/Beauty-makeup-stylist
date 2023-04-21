de158d064cd8e61be0535833af76c5b67a221b9c
TangHoangYen <yentang1212@gmail.com>
2023-04-22 06:00:17 +0700

    thay doi toan bo

--- js/about.js
+++ js/about.js
@@ -23,3 +23,21 @@ $(function () {
                 }
         });
 });
+var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: true,
+        },
+        loop: true,
+        autoplay: {
+                delay: 500,
+                disableOnInteraction: false,
+        },
+});
--- js/service_detail.js
+++ js/service_detail.js
@@ -1,54 +1,10 @@
-var arr_picture = [
-        "img_1/service-wedding/service-4.jpg",
-        "img_1/service-wedding/service-11.jpeg",
-        "img_1/service-wedding/service-8.jpg",
-        "img_1/service-wedding/service-9.jpg",
-];
-var index = 0;
-function next() {
-        index++;
-        if (index > 3) index = 0;
-        var show = document.getElementById("show");
-        show.src = arr_picture[index];
-}
-function back() {
-        index--;
-        if (index < 0) index = 3;
-        var show = document.getElementById("show");
-        show.src = arr_picture[index];
-}
-function picture_0() {
-        index = 0;
-        var show = document.getElementById("show");
-        show.src = arr_picture[index];
-}
-function picture_1() {
-        index = 1;
-        var show = document.getElementById("show");
-        show.src = arr_picture[index];
-}
-function picture_2() {
-        index = 2;
-        var show = document.getElementById("show");
-        show.src = arr_picture[index];
-}
-function picture_3() {
-        index = 3;
-        var show = document.getElementById("show");
-        show.src = arr_picture[index];
-}
-
 $(function () {
-        // $(".detail .service_picture .picture ul li").click(function () {
-        //         $imgSrc = $(this).attr("datasrc");
-        //         $(".detail .img  img").attr("src", $imgSrc);
-        //         $(this).addClass("active");
-        //         $(this).siblings().removeClass("active");
-        //         $(".add .product .img img").attr("src", $imgSrc);
-        // });
         $(".detail .service_picture .picture ul li").click(function () {
+                $imgSrc = $(this).attr("datasrc");
+                $(".detail .img  img").attr("src", $imgSrc);
                 $(this).addClass("active");
                 $(this).siblings().removeClass("active");
+                $(".add .product .img img").attr("src", $imgSrc);
         });
 
         $(".typeClick").click(function () {
@@ -56,7 +12,7 @@ $(function () {
                 $quality = $(this).parent().find("input").val();
                 if ($type == "plus") {
                         $quality++;
-                } else if ($type == "minus" && $quality > 1) {
+                } else if ($type == "minus" && $quality > 3) {
                         $quality--;
                 }
                 $(this).parent().find("input").val($quality);
@@ -67,19 +23,15 @@ $(function () {
                 $(this).siblings().removeClass("active");
                 $datastyle = $(this).attr("datastyle");
                 $(" h4 span").text($datastyle);
-                $(".desc span ").text($datastyle);
-        });
-        $(".trademarks ul li ").click(function () {
-                $(this).addClass("active");
-                $(this).siblings().removeClass("active");
-                $datalabel = $(this).attr("datalabel");
-                $(" h5 span").text($datalabel);
-                $(".trademark_s span ").text($datalabel);
+                $(" .add .product .content .desc span").text($datastyle);
         });
 
         $(".detail .trademarks ul li").click(function () {
                 $(this).toggleClass("active");
                 $(this).siblings().removeClass("active");
+                $datalabel = $(this).attr("datalabel");
+                $(" h5 span").text($datalabel);
+                $(" .add .product .content .labels span").text($datalabel);
         });
 
         $(".btn").click(function () {
